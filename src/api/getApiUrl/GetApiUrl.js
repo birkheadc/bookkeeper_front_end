0e5a832440f87184a17f6da851e4d1e44156f69b
Colby Birkhead <birkheadc@gmail.com>
2022-05-25 15:47:14 +0900

    fetch now uses https instead of plain http

--- src/api/getApiUrl/GetApiUrl.js
+++ src/api/getApiUrl/GetApiUrl.js
@@ -2,7 +2,7 @@ function getApiUrl() {
     if (process.env.NODE_ENV === 'development') {
         return process.env.REACT_APP_BOOKKEEPER_URL + "/api";
     }
-    return "http://" + window.location.host + "/api";
+    return "https://" + window.location.host + "/api";
 }
 
 export default getApiUrl;
