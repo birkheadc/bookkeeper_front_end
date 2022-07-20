abfc473718c71642c2a6c3508f6b60be1aa7ed08
Colby Birkhead <birkheadc@gmail.com>
2022-07-20 13:47:05 +0900

    remove console.log from retrieve-user-settings function

--- src/helpers/settings/RetrieveUserSettings.js
+++ src/helpers/settings/RetrieveUserSettings.js
@@ -3,9 +3,7 @@ import { Utils } from "..";
 function retrieveUserSettingByName(name) {
     try {
         const settings = JSON.parse(window.localStorage.getItem('settings'));
-        console.log(settings);
         const value = settings.userSettings[name].value;
-        console.log(value);
         return value;
     }
     catch {
